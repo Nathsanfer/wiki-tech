6ba1cc1a3dffd693d0266e0b5c8f137ee1e95909
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-08-18 20:07:18 -0300

    feat: create header component

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Bem-vindo ao meu projeto Next.js!</h1>
+      <p>Este é um projeto para mostrar tudo que eu sei sobre Next.js.</p>
+    </div>
+  );
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import styles from "./header.module.css";
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+        <div className={styles.logo}>
+          <h1>WIKI-TECH</h1>
+          <span>💻</span>
+        </div>
+        <nav className={styles.nav}>
+          <a href="#" className={styles.active}>
+            HOME
+          </a>
+        </nav>
+
+        <div className={styles.userActions}>
+          <button className={styles.notificationBtn}>🔔</button>
+        </div>
+      </header>
+    )
+}
+
+export default Header;
