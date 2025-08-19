a4bb45a195b0bee2e2113234312b8477c93b69ee
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-08-18 23:11:45 -0300

    feat: create sectionTitle component

--- src/app/pages/framework/page.jsx
+++ src/app/pages/framework/page.jsx
@@ -1,10 +1,17 @@
 import Header from "../../../components/Header";
+import SectionTitle from "../../../components/SectionTitle";
 
 export default function Framework() {
   return (
     <div>
 
       <Header />
+      <SectionTitle
+        linkImage="https://cdn-icons-png.flaticon.com/128/3906/3906805.png"
+        imageAlt="Engrenagem"
+        title="Frameworks"
+        description="É uma .blablabla didididi dado subida canção"
+      />
       <h1>Framework</h1>
 
     </div>
---
+++ src/components/SectionTitle/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./sectionTitle.module.css";
+
+const SectionTitle = ({ linkImage, imageAlt, title, description }) => {
+    return (
+        <div className={styles.infoCard}>
+            <div className={styles.infoCardBar}></div>
+            <img src={linkImage} alt={imageAlt} className={styles.infoCardIcon} />
+            <div className={styles.infoCardContent}>
+                <h2 className={styles.infoCardTitle}>{title}</h2>
+                <p className={styles.infoCardText}>{description}</p>
+            </div>
+        </div>
+    )
+}
+
+export default SectionTitle;
